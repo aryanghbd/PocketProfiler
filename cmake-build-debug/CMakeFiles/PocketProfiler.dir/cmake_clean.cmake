50571028e198bd91a629387fe0a27a76c43829e8
aryanghbd <54407419+aryanghbd@users.noreply.github.com>
2022-11-26 20:55:18 +0000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/PocketProfiler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PocketProfiler.dir/main.cpp.o"
+  "PocketProfiler"
+  "PocketProfiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PocketProfiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
